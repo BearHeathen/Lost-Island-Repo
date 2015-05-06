d9231ae1a2b68cff82a92cec517a00ba1cf129e0
Jeffrey D Page <jdpage5794@yahoo.com>
2015-05-05 22:06:00 -0500

    Player Death and Re spawn
    Added a instant death/teleport when player touches water/lava, currently
    working on player lives and updating player health and lives upon player
    death.

---
+++ Lost Island/Assets/Scripts/OnWaterEnter.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OnWaterEnter : MonoBehaviour {
+
+	private GameObject Water;
+	public Transform Player;
+	public Transform Destination;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	//void Awake () {}
+
+	void OnTriggerEnter (Collider other)
+	{
+		if (other.tag == "Player") {
+			Water = GameObject.Find("Water");
+			KIllPlayer();
+		}
+	}
+
+	void KIllPlayer()
+	{
+		Player.transform.position = Destination.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+}
