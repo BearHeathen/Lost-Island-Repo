809e1d20bd0f214801b5ad9e1830a023f79a7fc2
Jeffrey D Page <jdpage5794@yahoo.com>
2015-05-05 23:59:55 -0500

    Cont of last commit

--- Lost Island/Assets/Scripts/OnLavaEnter.cs
+++ Lost Island/Assets/Scripts/OnLavaEnter.cs
@@ -4,18 +4,18 @@ using System.Collections;
 public class OnLavaEnter : MonoBehaviour {
 	private GameObject LavaBox;
 	PlayerHealth playerHealth;
-	Transform Player;
+	public Transform Player;
 	public Transform Destination;
 	
 	// Use this for initialization
 	void Start () {
-		
+		playerHealth = GetComponent<PlayerHealth>();
 	}
 	
 	void Awake () 
 	{
-		Player = GameObject.FindGameObjectWithTag("Player").transform;
-		playerHealth = Player.GetComponent <PlayerHealth> ();
+		//Player = GameObject.FindGameObjectWithTag("Player").transform;
+		//playerHealth = Player.GetComponent <PlayerHealth> ();
 	}
 	
 	void OnTriggerEnter (Collider other)
@@ -29,13 +29,13 @@ public class OnLavaEnter : MonoBehaviour {
 	void KIllPlayer()
 	{
 		Player.transform.position = Destination.position;
-		if (playerHealth.currentHealth < 100) {
-			playerHealth.currentHealth = playerHealth.startingHealth;
-		}
+		//if (playerHealth.currentHealth < 100) {
+			//playerHealth.currentHealth = playerHealth.startingHealth;
+		//}
 	}
 	
 	// Update is called once per frame
 	void Update () {
-
+		playerHealth.currentHealth = playerHealth.startingHealth;
 	}
 }
